770fecbc6a03309a6253994736f7e2f8d3c3bf83
@vbandrade <vbandrade@gmail.com>
2018-03-03 21:59:04 -0300

    corrigindo nome da classe e remoendo comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import "./screens/login_page.dart";
-import "./screens/home_page.dart";
+import "./screens/click_counter_page.dart";
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
       home: new LoginPage(),
       theme: new ThemeData(primarySwatch: Colors.amber),
       routes: <String, WidgetBuilder>{
         '/clicker': (BuildContext context) => new ClickCounter(),
-        // '/b': (BuildContext context) => new MyPage(title: 'page B'),
-        // '/c': (BuildContext context) => new MyPage(title: 'page C'),
       },
     );
   }
